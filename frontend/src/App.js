4fd6c2eca56a68dbfb93a20dc9f4eeb737174814
Manish <manish.quarec@gmail.com>
2023-01-20 10:46:43 +0530

    new

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import Nav from './Component/Nav/Nav';
+import BreakingNews from './Component/BreakingNews/BreakingNews';
+import LeftMenuBar from './Component/LeftMenuBar/LeftMenuBar';
+import AppStore from './Component/AppStore/AppStore';
+import VideoSlider from './Component/VideoSlider/VideoSlider';
+import ImageData from './Component/ImageData/ImageData';
+import NewsBlock from './Component/NewsBlock/NewsBlock';
+import SocialMediaLeft from './Component/SocialMediaLeft/SocialMediaLeft';
+import RightPhotoImage from './Component/RightPhotoImage/RightPhotoImage';
+import ShortNews from './Component/ShortNews/ShortNews';
+import HomePage from './Pages/HomePage/HomePage';
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage/>
+   
+    </div>
+  );
+}
+
+export default App;
