443de0ba9c1afe380c7eac84a83dca00153a7657
ManishQuarec <119395712+ManishQuarec@users.noreply.github.com>
2023-02-22 17:48:29 +0530

    new

--- frontend/src/Component/FullNews/FullNews.jsx
+++ frontend/src/Component/FullNews/FullNews.jsx
@@ -5,9 +5,7 @@ import { Route, Routes, BrowserRouter, useParams } from "react-router-dom";
 import axios from "axios";
 import NewsBlock from "../NewsBlock/NewsBlock";
 function MyComponent({ htmlContent }) {
-  return (
-    <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
-  );
+  return <div dangerouslySetInnerHTML={{ __html: htmlContent }} />;
 }
 
 function FullNews() {
@@ -16,66 +14,71 @@ function FullNews() {
   const [newsData, setNewsData] = useState({});
   const [image, setImage] = useState("");
   const [tittle, setTittle] = useState("");
+  const [subTittle, setSubTittle] = useState("");
   const [news, setNews] = useState("");
+  const [colored, setColored] = useState("");
 
-//  const data = (dat) => {
-//   React.createElement(
-  
-//   {dat}
-//   )
-//  }
+  //  const data = (dat) => {
+  //   React.createElement(
 
+  //   {dat}
+  //   )
+  //  }
 
+  // function createMarkup(text) { return {__html: text}; };
 
-// function createMarkup(text) { return {__html: text}; };
-
-  console.log( news);
+  console.log(news);
   useEffect(() => {
     axios
       .post(process.env.REACT_APP_API_BASE_URL + "/allNewsDataId", {
         data: userId,
       })
       .then(async (response) => {
-        console.log(response.data.response[0].Path);
+        console.log("new", response.data.response[0]);
         await setNewsData(response.data.response);
         await setImage(response.data.response[0].Path);
         await setTittle(response.data.response[0].NewsTittle);
         await setNews(response.data.response[0].News);
-
+        await setSubTittle(response.data.response[0].NewsSubTittle);
+        await setColored(response.data.response[0].Colored);
         // console.log(response.data.response);
       });
   }, []);
-
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
   return (
     <>
-    <div className="datt">
-      <div className="FullNews">
-        {/* <a href="/State">
+      <div className="datt">
+        <div className="FullNews">
+          {/* <a href="/State">
           <i className="fa fa-arrow-left" aria-hidden="true"></i>&nbsp;&nbsp;
           ગુજરાતી / સમાચાર / સ્થાનિક / ગુજરાત / ગાંધીનગર / 
         </a> */}
+          <div className="NewsContent">
+            <h1>
+              <font style={{ color: colored ? colored : "#000000" }}>
+                {" "}
+                {tittle.slice(0, 52)}{" "}
+              </font>{" "}
+              {subTittle.split(" ").splice(0, 20).join(" ")}
+            </h1>
+          </div>
+        </div>
+        <div className="ImgSection">
+          <img src={process.env.REACT_APP_API_URL + `${image}`} alt="" />
+        </div>
         <div className="NewsContent">
-        <h1><MyComponent htmlContent={tittle}/></h1>
+          {/* <h1>{tittle}</h1> */}
+          {/* <>{news}</> */}
+          <MyComponent htmlContent={news} />
+          {/* <data dat={news}/> */}
+        </div>
+        <NewsBlock style={{ width: "auto" }} value={{ unique: true }} />
       </div>
-      </div>
-      <div className="ImgSection">
-        <img src={process.env.REACT_APP_API_URL + `${image}`} alt="" />
-      </div>
-      <div className="NewsContent">
-        {/* <h1>{tittle}</h1> */}
-        {/* <>{news}</> */}
-        <MyComponent htmlContent={news}/>
-    {/* <data dat={news}/> */}
-      </div>
-      <NewsBlock style={{width:"auto"}} value={{ unique: true }} />
-      </div>
-      
     </>
   );
 }
 
-export { FullNews, MyComponent};
+export { FullNews, MyComponent };
